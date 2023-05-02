e5a853a93f6351d4db3821c6f70d4e77a7ee1d2b
Bannon Smith <shwiftycouch@gmail.com>
2023-05-02 14:45:40 -0600

    second commit

--- assessment-backend/server/index.js
+++ assessment-backend/server/index.js
@@ -7,8 +7,15 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const { getCompliment } = require('./controller')
 const { getFortune } = require('./controller')
+
 app.get("/api/compliment", getCompliment);
 app.get('/api/fortune', getFortune)
 app.listen(4000, () => console.log("Server running on 4000"));
