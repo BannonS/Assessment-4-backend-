272591682e843d59ba4235eb15c101e8c8057dde
Bannon Smith <shwiftycouch@gmail.com>
2023-05-02 15:45:20 -0600

    pudding

--- assessment-backend/server/index.js
+++ assessment-backend/server/index.js
@@ -18,5 +18,5 @@ const { getFortune } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
 app.get('/api/fortune', getFortune);
-//app.listen(4000, () => console.log("Server running on 4000"));//
+app.listen(4000, () => console.log("Server running on 4000"));
 
